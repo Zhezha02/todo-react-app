6bc898e20fa1bbed2ac2386426016bd8b6710389
Evgeniya Kirtoka <evgeniyakirtoka@gmail.com>
2021-03-09 18:50:25 +0200

    Fix toDo validation schema

--- src/utils/validationSchemas.js
+++ src/utils/validationSchemas.js
@@ -1,6 +1,5 @@
 import * as Yup from 'yup';
 
-export const TO_DO_LIST_INPUT = Yup.string().matches(
-  /^(\w#?!@$ %^&*-){2,}$/,
-  `Task mustn't be an empty field`
-);
+export const TO_DO_INPUT_SCHEMA = Yup.object({
+  toDo: Yup.string().matches(/^[\w ]+$/, "Task mustn't be an empty field"),
+});
