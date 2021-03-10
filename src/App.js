3a1ff0cabd1bd1ac09200b4db5809bc28d9f14fc
Evgeniya <evgeniyakirtoka@gmail.com>
2021-03-10 15:35:49 +0200

    Add ToDoListPage styles

--- src/App.js
+++ src/App.js
@@ -1,40 +1,8 @@
 import React, { useState } from 'react';
-import TaskInput from './components/TaskInput';
-import ToDoList from './components/ToDoList';
+import ToDoListPage from './pages/ToDoListPage';
 
 const App = props => {
-  const [tasks, setTasks] = useState(new Map());
-
-  const addTasks = ({ toDo }) => {
-    const newTask = {
-      taskValue: toDo,
-      isDone: false,
-    };
-
-    setTasks(new Map([...tasks]).set(Date.now(), newTask));
-  };
-
-  const switchIsDone = id => {
-    const task = tasks.get(id);
-    task.isDone = !task.isDone;
-  };
-
-  const deleteTask = id => {
-    const newTasks = new Map([...tasks]);
-    newTasks.delete(id);
-    setTasks(newTasks);
-  };
-
-  return (
-    <div>
-      <TaskInput onSubmit={addTasks} />
-      <ToDoList
-        toDoInputs={tasks}
-        switchIsDone={switchIsDone}
-        deleteTask={deleteTask}
-      />
-    </div>
-  );
+  return <ToDoListPage />;
 };
 
 export default App;
---
+++ src/pages/ToDoListPage/index.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import TaskInput from '../../components/TaskInput';
+import ToDoList from '../../components/ToDoList';
+import styles from './ToDoListPage.module.scss';
+
+const ToDoListPage = props => {
+  const [tasks, setTasks] = useState(new Map());
+
+  const addTasks = ({ toDo }) => {
+    const newTask = {
+      taskValue: toDo,
+      isDone: false,
+    };
+
+    setTasks(new Map([...tasks]).set(Date.now(), newTask));
+  };
+
+  const switchIsDone = id => {
+    const task = tasks.get(id);
+    task.isDone = !task.isDone;
+  };
+
+  const deleteTask = id => {
+    const newTasks = new Map([...tasks]);
+    newTasks.delete(id);
+    setTasks(newTasks);
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.heading}>to-do list</h1>
+      <div className={styles.container}>
+        <TaskInput onSubmit={addTasks} />
+        <ToDoList
+          toDoInputs={tasks}
+          switchIsDone={switchIsDone}
+          deleteTask={deleteTask}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ToDoListPage;
